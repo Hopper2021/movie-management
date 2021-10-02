f7c64e99cb29a00c469d82ab1e178479d9de7f29
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-10-01 19:15:42 -0500

    history.push to detail page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,9 +2,10 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import MovieItemDetails from '../MovieItemDetails/MovieItemDetails.jsx'
+import  { useHistory } from 'react-router-dom';
 
 function MovieList() {
-
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
@@ -12,15 +13,21 @@ function MovieList() {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+    const moveToDetailPage = () => {
+        history.push('/details');
+    }
+
     return (
         <main>
             <h1>MovieList</h1>
             <section className="movies">
-                {/* Testing to see if description is coming back */}
+                {/* Testing to see if description is coming back, Success! */}
                 {/* {JSON.stringify(movies)} */}
                 {movies.map(movie => {
                     return (
-                        <MovieItemDetails key={movie.id} movie={movie}/>
+                        <MovieItemDetails key={movie.id} movie={movie}
+                        onClick={moveToDetailPage}
+                        />
                     );
                 })}
             </section>
