0de9835a53a31459f0b42e0637abfd383b4902bd
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-10-03 18:39:36 -0500

    Added genre entry to add movie page

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -25,7 +25,7 @@ function AddMovie() {
     }
 
     const handleGenreChange = (event) => {
-        setNewMovie({...newMovie, genre: event.target.value});
+        setNewMovie({...newMovie, genre: event.target.value });
     }
 
     const addNewMovie = (event) => {
@@ -42,40 +42,32 @@ function AddMovie() {
                 <input required
                     placeholder="Title"
                     value={newMovie.title}
-                    onChange={handleTitleChange}/><br />
+                    onChange={handleTitleChange}/>
+                <br />
                 <input required
                     placeholder="Poster Image"
                     value={newMovie.poster}
-                    onChange={handlePosterChange}/><br /><br /> 
-                <label for="genres">Choose a genre</label><br />
-                {/* {JSON.stringify(genres)}<br /> */}
+                    onChange={handlePosterChange}/>
+                <br />
+                <br /> 
+                <label for="genres">
+                    Choose a genre
+                </label>
+                    <br />
                 <select id="genres" name="genres" label="genres"
-                    onSelect={handleGenreChange}>
+                onChange={(event) => handleGenreChange(event)}>
                     {genres.map(({name}) => (
-                        <option value={name}>{name}</option>
+                        <option key={name.id} value={name}>{name}</option>
                     ))}
-                    {/* <option value="Adventure">Adventure</option>
-                    <option value="Animated">Animated</option>
-                    <option value="Biographical">Biographical</option>
-                    <option value="Comedy">Comedy</option>
-                    <option value="Disaster">Disaster</option>
-                    <option value="Drama">Drama</option>
-                    <option value="Epic">Epic</option>
-                    <option value="Fantasy">Fantasy</option>
-                    <option value="Musical">Musical</option>
-                    <option value="Romantic">Romantic</option>
-                    <option value="Science Fiction">Science Fiction</option>
-                    <option value="Space-Opera">Space-Opera</option>
-                    <option value="Superhero">Superhero</option> */}
-                </select><br /><br />
+                </select>
+                <br />
+                <br />
                 <textarea cols="40" rows="5"
                     placeholder="Description"
                     value={newMovie.description}
                     onChange={handleDescriptionChange}/><br />
                 <Router>
-                    <Link to="/">
-                    <button>Cancel</button>
-                    </Link>
+                    <Link to="/">Cancel</Link>
                 </Router>
                     <button type="submit">Save</button>
             </form>
