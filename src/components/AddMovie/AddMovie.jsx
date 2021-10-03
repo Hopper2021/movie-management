093d408b53e03df298fe54da2e0ffbfec663ba90
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-10-03 16:28:11 -0500

    Added dispatch and useHistory to store input values in Add Movie Form

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,36 +1,74 @@
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import React, { useState }from 'react';
+import { useDispatch } from 'react-redux';
+import  { useHistory } from 'react-router-dom';
+
+
 
 function AddMovie() {
+    const dispatch = useDispatch();
+    const history = useHistory();
 
-    const handleSubmit = () => {
-        
+    let [newMovie, setNewMovie] = useState(
+        { title: '', poster: '', description: '', genre: '' }
+    );
+    
+    const handleTitleChange = (event) => {
+        setNewMovie({...newMovie, title: event.target.value});
+    }
+
+    const handlePosterChange = (event) => {
+        setNewMovie({...newMovie, poster: event.target.value});
+    }
+
+    const handleDescriptionChange = (event) => {
+        setNewMovie({...newMovie, description: event.target.value});
+    }
+
+    const handleGenreChange = (event) => {
+        setNewMovie({...newMovie, genre: event.target.value});
+    }
+
+    const addNewMovie = (event) => {
+        event.preventDefault();
+        dispatch({ type: 'POST_NEW_MOVIE', payload: newMovie });
+        setNewMovie({ title: '', poster: '', description: '', genre: '' });
+        console.log('This is the new movie - ', newMovie);
+        history.push('/');
     }
 
     return(
         <>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={addNewMovie}>
                 <input required
-                    placeholder="Title"/><br />
+                    placeholder="Title"
+                    value={newMovie.title}
+                    onChange={handleTitleChange}/><br />
                 <input required
-                    placeholder="Poster Image"/><br /><br /> 
+                    placeholder="Poster Image"
+                    value={newMovie.poster}
+                    onChange={handlePosterChange}/><br /><br /> 
                 <label for="genres">Choose a genre</label><br />
-                <select id="genres" name="genres" label="genres">
-                    <option>Adventure</option>
-                    <option>Animated</option>
-                    <option>Biographical</option>
-                    <option>Comedy</option>
-                    <option>Disaster</option>
-                    <option>Drama</option>
-                    <option>Epic</option>
-                    <option>Fantasy</option>
-                    <option>Musical</option>
-                    <option>Romantic</option>
-                    <option>Science Fiction</option>
-                    <option>Space-Opera</option>
-                    <option>Superhero</option>
+                <select id="genres" name="genres" label="genres"
+                    onChange={handleGenreChange}>
+                    <option value="Adventure">Adventure</option>
+                    <option value="Animated">Animated</option>
+                    <option value="Biographical">Biographical</option>
+                    <option value="Comedy">Comedy</option>
+                    <option value="Disaster">Disaster</option>
+                    <option value="Drama">Drama</option>
+                    <option value="Epic">Epic</option>
+                    <option value="Fantasy">Fantasy</option>
+                    <option value="Musical">Musical</option>
+                    <option value="Romantic">Romantic</option>
+                    <option value="Science Fiction">Science Fiction</option>
+                    <option value="Space-Opera">Space-Opera</option>
+                    <option value="Superhero">Superhero</option>
                 </select><br /><br />
                 <textarea cols="40" rows="5"
-                    placeholder="Description"/><br />
+                    placeholder="Description"
+                    value={newMovie.description}
+                    onChange={handleDescriptionChange}/><br />
                 <Router>
                     <Link to="/">
                     <button>Cancel</button>
